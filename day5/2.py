26453a5d874439207e25b851a9704473a1ed84ec
petar-savov <p.savov@outlook.com>
2020-12-05 21:16:14 +0000

    Solve in one piece

--- day5/2.py
+++ day5/2.py
@@ -1,15 +1,14 @@
+import re
+
 with open("input.txt") as f:
     inp = f.readlines()
 
 inp = [line.strip() for line in inp]
 
-def get_id(ticket: str):
-    row = ticket[:7].replace("F","0").replace("B","1")
-    col = ticket[7:].replace("L","0").replace("R","1")
-    row = int(row,2)
-    col = int(col,2)
-
-    return row*8 + col
+def get_id(bpass: str):
+    bpass = re.sub("F|L", "0", bpass)
+    bpass = re.sub("B|R", "1", bpass)
+    return int(bpass,2)
 
 ids = [get_id(x) for x in inp]
 
