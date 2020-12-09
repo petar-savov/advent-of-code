a4bfb74ed4d9e7a32a46433f329b3214bf1369ce
petar-savov <p.savov@outlook.com>
2020-12-09 22:51:19 +0000

    Python formatting

--- day9/main.py
+++ day9/main.py
@@ -1,15 +1,15 @@
 with open("input.txt") as f:
     inp = [int(x.strip()) for x in f.readlines()]
 
-#part 1
-for i,n in enumerate(inp):
-    if i>24:
-        prev = inp[i-25:i+1]
+# part 1
+for i, n in enumerate(inp):
+    if i > 24:
+        prev = inp[i - 25 : i + 1]
 
         pair_sums = set()
         for j in prev:
             for k in prev:
-                pair_sums.add(j+k)
+                pair_sums.add(j + k)
 
         if n not in pair_sums:
             answer = n
@@ -17,22 +17,20 @@ for i,n in enumerate(inp):
 
 print(answer)
 
-#part 2
+# part 2
 target = answer
 
-start, end = 0,1 
+start, end = 0, 1
 csum = inp[0] + inp[1]
 
-while csum!=target:
-    while(csum<target):
+while csum != target:
+    while csum < target:
         end += 1
-        csum+=inp[end]
+        csum += inp[end]
+        
+    while csum > target:
+        csum -= inp[start]
+        start += 1
 
-    while(csum>target):
-        csum-=inp[start]
-        start+=1
-
-print(min(inp[start:end])+max(inp[start:end]))
-
-
+print(min(inp[start:end]) + max(inp[start:end]))
 
