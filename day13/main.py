3afedd98cddab31d53ab639d7e4940df365c681c
petar-savov <p.savov@outlook.com>
2020-12-13 11:16:59 +0000

    Day 13 part 1

---
+++ day13/main.py
@@ -0,0 +1,19 @@
+import re
+
+with open("input.txt") as f:
+    inp = [line.strip() for line in f.readlines()]
+
+start = int(inp[0])
+buses = re.findall("\d+", inp[1])
+buses = [int(x) for x in buses]
+
+depart = max(buses) + start
+for bus in buses:
+    dep = start - start%bus + bus
+    if dep < depart:
+        depart = dep
+        bus_id = bus
+
+print((depart-start)*bus_id)
+
+
