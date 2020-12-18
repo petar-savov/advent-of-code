3ae8dc1916a5dbcde335a7b2100dba862e5cc83f
petar-savov <p.savov@outlook.com>
2020-12-18 10:41:23 +0000

    Complete day 16 part 2 solution

--- day16/2.py
+++ day16/2.py
@@ -8,13 +8,14 @@ constraints, your_ticket, tickets = inp.split("\n\n")
 fields = re.findall("(.*):", constraints)
 lims = re.findall(" (\d+)-(\d+) or (\d+)-(\d+)", constraints)
 lims = [((int(x[0]), int(x[1])), (int(x[2]), int(x[3]))) for x in lims]
+ref = dict(zip(fields, lims))
 
 tickets = tickets.split("\n")[1:-1]
 tickets = [[int(x) for x in line.split(",")] for line in tickets]
 
 
 def check(n, lim):
-    if (n >= lim[0][0] and n <= lim[0][1]) or (n >= lim[1][0] and n <= lim[1][1]):
+    if (lim[0][0] <= n <= lim[0][1]) or (lim[1][0] <= n <= lim[1][1]):
         return True
     return False
 
@@ -27,8 +28,34 @@ for ticket in tickets:
         if any(check(num, lim) for lim in lims):
             pass
         else:
-
             valid = False
     if valid:
         to_keep.append(ticket)
 
+poss = {}
+for i in range(len(to_keep[0])):
+    for name in ref:
+        if all(check(t[i], ref[name]) for t in to_keep):
+            if name not in poss:
+                poss[name] = set([i])
+            else:
+                poss[name].add(i)
+
+
+res = {}
+while len(res) < len(poss):
+    for key in poss:
+        if len(poss[key]) == 1:
+            ind = poss[key].pop()
+            res[key] = ind
+            for k2 in poss:
+                poss[k2].discard(ind)
+
+your_ticket = [int(x) for x in your_ticket.split("\n")[1].split(",")]
+
+prod = 1
+for key in res:
+    if key.split()[0] == "departure":
+        prod *= your_ticket[res[key]]
+
+print(prod)
