ec999e47def1ec6d5a3e1c72b4dbd30293871efa
petar-savov <p.savov@outlook.com>
2020-12-21 23:48:58 +0000

    Day 20 part 2 - populate first row

--- day20/main.py
+++ day20/main.py
@@ -19,14 +19,14 @@ for tile in inp:
 
 def get_flips(arr: np.array) -> list:
 
-    get_flips = [arr]
-    get_flips.extend([np.rot90(arr, k) for k in [1, 2, 3]])
-    get_flips.extend([np.flip(arr, axis) for axis in [0, 1]])
+    flips = [arr]
+    flips.extend([np.rot90(arr, k) for k in [1, 2, 3]])
+    flips.extend([np.flip(arr, axis) for axis in [0, 1]])
 
-    get_flips.append(np.rot90(np.fliplr(arr)))
-    get_flips.append(np.rot90(np.flipud(arr)))
+    flips.append(np.rot90(np.fliplr(arr)))
+    flips.append(np.rot90(np.flipud(arr)))
 
-    return get_flips
+    return flips
 
 
 def get_edges(arr: np.array) -> list:
@@ -56,8 +56,50 @@ for k in edges:
                 s += sum([np.array_equal(edge, other) for other in edges[j]])
     matches[k] = s
 
+corners = []
 prod = 1
 for k in matches:
     if matches[k] == 4:
+        corners.append(k)
         prod *= k
 print(prod)
+
+# part 2
+# corners = [2749, 2713, 1487, 1063]
+
+tl_index = corners[0]
+topleft = tiles[tl_index]
+tl_edges = get_edges(topleft)
+
+for edge in tl_edges:
+    s = 0
+    for j in edges:
+        if j != 2749:
+            s += sum([np.array_equal(edge, other) for other in edges[j]])
+    if s == 0:
+        print(edge)
+
+img = np.rot90(topleft, k=2)
+tiles.pop(2749)
+
+while True:
+    for tile in tiles:
+        flips = get_flips(tiles[tile])
+        br = False
+        for flip in flips:
+            if np.array_equal(img[:, -1], flip[:, 0]):
+                img = np.hstack((img, flip))
+                br = True
+                to_pop = tile
+                break
+        if br:
+            break
+    tiles.pop(to_pop)
+    if to_pop in corners[1:]:
+        tr_index = to_pop
+        break
+
+# img is now the first row
+expand = np.empty((110, 120))
+img = np.vstack((img, expand))
+
